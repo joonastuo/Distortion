2ff464fc0520be2be940d8f680d6eaedd1fbaa24
Joonas Tuovinen <joonas.tuovinen@aalto.fi>
2019-03-09 18:38:19 +0200

    oversampling used and dsp::waveshper used for waveshaping. Wet/dry does not work yet.

--- Source/Distortion.h
+++ Source/Distortion.h
@@ -20,11 +20,26 @@ class Distortion
 public:
     Distortion(AudioProcessorValueTreeState& vt);
     ~Distortion();
-	float process(float sample);
-	float expDistortion(float sample);
+	void prepare(dsp::ProcessSpec);
+	void reset();
+	void process(dsp::ProcessContextReplacing<float> block);
+	void updateParameters();
 
 private:
+	// Private methods
+	float expDistortion(float sample);
+
+	// Private variables
 	AudioProcessorValueTreeState& mParameters;
+	static constexpr size_t numWaveShapers = 1;
+	dsp::WaveShaper<float> waveShapers[numWaveShapers];
+	std::unique_ptr<dsp::Oversampling<float>> mOversampling;
+	dsp::Gain<float> inputVolume, outputVolume;
+
+	float mSampleRate = 44100.f;
+	uint32 mMaxBlockSize = 512;
+	uint32 mNumChannels = 2;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Distortion)
 };
